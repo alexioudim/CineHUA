cbf04aa46cc0e98881e9adc37d0ff546579bd2f4
Alexiou Dimitrios <it2021002@hua.gr>
2025-02-25 15:04:16 +0200

    Initiate Project

---
+++ src/main/java/gr/dit/hua/CineHua/CineHuaApplication.java
@@ -0,0 +1,13 @@
+package gr.dit.hua.CineHua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineHuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineHuaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/dit/hua/CineHua/CineHuaApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.dit.hua.CineHua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineHuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
