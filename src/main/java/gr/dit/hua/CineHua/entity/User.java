c28aa21b8c31d9e33b3e3f5101aae6c2c9b83bd2
Dimitrios Alexiou <it2021002@hua.gr>
2025-02-26 12:31:39 +0200

    Created database and added some entities

---
+++ src/main/java/gr/dit/hua/CineHua/entity/User.java
@@ -0,0 +1,56 @@
+package gr.dit.hua.CineHua.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String surName;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private int phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
