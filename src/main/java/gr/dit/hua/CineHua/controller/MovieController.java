be00ef09b622512f4c168d330cfea42ec1e20250
Alexiou Dimitrios <it2021002@hua.gr>
2025-03-12 00:51:59 +0200

    Added OMDb API key and created a demo

--- src/main/java/gr/dit/hua/CineHua/controller/MovieController.java
+++ src/main/java/gr/dit/hua/CineHua/controller/MovieController.java
@@ -3,10 +3,8 @@ package gr.dit.hua.CineHua.controller;
 import gr.dit.hua.CineHua.entity.Movie;
 import gr.dit.hua.CineHua.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Date;
 @RestController
@@ -18,7 +16,15 @@ public class MovieController {
 
     @PostMapping("/new")
     public Movie createMovie(@RequestBody Movie newMovie) {
-
         return movieService.saveMovie(newMovie);
+    }
+
+    @GetMapping("/new/{imdb_id}")
+    public String createMovie(@PathVariable String imdb_id) {
+        String uri = "http://www.omdbapi.com/?i=";
+        String api_key = "&apikey=8657bea9";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri + imdb_id + api_key, String.class);
+        return result;
     }
 }
